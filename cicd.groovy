ce9c806ab9d2e9f809e2dd86034997052272808d
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:02 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/wharfport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/wharfport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/wharfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A porting tool for Go packages on z/OS' ), string(name: 'NODE_LABEL', value: "v3r1")]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/wharfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A porting tool for Go packages on z/OS' ), string(name: 'NODE_LABEL', value: "v3r1")]
   }
 }
